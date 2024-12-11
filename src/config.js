6df1fc2616120c86aeb75d8c7d100fc97ceecf61
Jagadeeshreddymeruva <jagadeeshreddy.meruva@slu.edu>
2024-12-11 05:09:28 +0000

    Week7lab

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://vigilant-spoon-x5v477jx6w7jcp54x-3080.app.github.dev";

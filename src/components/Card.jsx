3f3ffb0cd927116bc6de9d7a235d235683a80a7c
steven-muthu <steven.muthu@slu.edu>
2024-12-15 03:41:34 +0000

    week 00007 lab

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Card = ({description, alt_description, id, _id, user, urls, likes}) => {
 
   const style = {
-    backgroundImage: `url(${urls.small})`
+    backgroundImage: url(${urls.small})
   }
   
   return (
     <div className="fl w-50 w-25-m w-20-l pa2">
-      <Link to={`/product/${_id}`} className="db link dim tc"> 
+      <Link to={/product/${_id}} className="db link dim tc"> 
         <div style={style} alt="" class="w-100 db outline black-10 h4 cover"></div>
         <dl className="mt2 f6 lh-copy">
           <dt className="clip">Title</dt>
